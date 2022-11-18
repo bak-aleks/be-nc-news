65b5a4e38ac579a8a1e86dd6be2b53387d7f8bf1
Aleks Bak <bak.aleksandra99@gmail.com>
2022-11-18 08:56:10 +0000

    new test added for comment_count, instead of modifying original

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -153,6 +153,22 @@ describe('3.GET/api/articles/:article_id', ()=>{
                 author: "butter_bridge",
                 body: "I find this existence challenging",
                 created_at: '2020-07-09T20:11:00.000Z',
+                votes: 100
+            })
+        })
+    })
+    test('status 200, should respond with corresponding object with comment_count key', ()=>{
+        return request(app)
+        .get('/api/articles/1')
+        .expect(200)
+        .then((response)=>{
+            expect(response.body.article).toMatchObject({
+                article_id:1,
+                title: "Living in the shadow of a great man",
+                topic: "mitch",
+                author: "butter_bridge",
+                body: "I find this existence challenging",
+                created_at: '2020-07-09T20:11:00.000Z',
                 votes: 100,
                 comment_count:11
             })
